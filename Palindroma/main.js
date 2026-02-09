7074940f37dab6a12c0c6852b87d51f35e5d1081
carminenicotera <carmine.91.n@hotmail.it>
2026-02-09 15:42:19 +0100

    add reversedWord function to Palindroma

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -1,1 +1,12 @@
-console.log('prova1');
+//chiedere all'utente di inserire una parola
+const word = prompt('Inserisci una parola: ');
+
+//funzione che inverte la parola inserita
+function reversedWord (word) {
+  let reversedWord = '';
+  for (let i = word.length - 1; i >= 0; i--) {
+    const char = word[i];
+    reversedWord += char;
+  }
+  console.log(reversedWord);
+}
